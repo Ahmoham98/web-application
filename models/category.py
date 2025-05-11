155e68e3e62b5ce8a75503704c0fbf5eb7795ac2
Ahmoham98 <electricallover45@gmail.com>
2025-05-11 10:35:11 +0330

    creates category postgres database model

---
+++ models/category.py
@@ -0,0 +1,33 @@
+from typing import TYPE_CHECKING
+from datetime import datetime, timezone
+from sqlmodel import SQLModel, Field, Relationship, Column
+import sqlalchemy.dialects.postgresql as pg
+import uuid
+
+# Categories table
+
+class Categories(SQLModel, table=True):
+    
+    __tablename__ = "categories"
+    
+    uid: uuid.UUID | None = Field(sa_column=Column(
+        pg.UUID,
+        nullable=False,
+        primary_key=True,
+        default=uuid.uuid4,
+        index=True,
+        unique=True
+    ))
+    title: str = Field(
+        pg.VARCHAR(20),
+        index=True,
+    )
+    description: str | None = Field(sa_column=Column(
+        pg.VARCHAR(100),
+        default="no description "
+    ))
+    created_at: datetime = Field(
+        sa_column=Column(
+            pg.TIMESTAMP,
+            default=datetime.now(timezone.utc)
+    ))
