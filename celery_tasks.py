ef8cfdc625e1b5b224e37795035694943fe2be41
Ahmoham98 <electricallover45@gmail.com>
2025-05-11 10:29:31 +0330

    creates celery task to handle sending email as background task, finally make project be able to handle the requests faster

---
+++ celery_tasks.py
@@ -0,0 +1,20 @@
+from typing import List
+from celery import Celery
+from mail import create_message, mail
+from asgiref.sync import async_to_sync
+
+c_app = Celery()
+
+c_app.config_from_object('configure')
+
+@c_app.task()
+def send_email(recipients: List[str], subject: str, body: str):
+    message = create_message(
+        recipients=recipients,
+        subject=subject,
+        body=body
+    )
+    
+    async_to_sync(mail.send_message)(message)
+
+
