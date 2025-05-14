55352da892178664aee8cd85dc826f4dd9233f15
Ahmoham98 <electricallover45@gmail.com>
2025-05-14 04:53:06 +0330

    set configure.py to read data's from .env file

--- configure.py
+++ configure.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET: str
     JWT_ALGORITHM: str
-    """REDIS_URL: str = "redis://localhost:6379/0"
+    """REDIS_URL: str = "redis://localhost:6379/0"""
     MAIL_USERNAME: str
     MAIL_PASSWORD : str
     MAIL_FROM = str
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     MAIL_STARTTLS: bool = True
     MAIL_SSL_TLS: bool = False
     USE_CREDENTIALS: bool = True
-    VALIDATE_CERTS: bool = True"""
+    VALIDATE_CERTS: bool = True
     DOMAIN: str
     
     model_config = SettingsConfigDict(
