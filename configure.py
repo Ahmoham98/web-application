da08935879b91cda43dc857692c99ed1327d4c0e
Ahmoham98 <electricallover45@gmail.com>
2025-05-14 06:42:14 +0330

    debugs configure.py

--- configure.py
+++ configure.py
@@ -1,26 +1,27 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import EmailStr
 
+
 class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET: str
     JWT_ALGORITHM: str
     """REDIS_URL: str = "redis://localhost:6379/0"""
-    MAIL_USERNAME: str
-    MAIL_PASSWORD : str
-    MAIL_FROM = str
-    MAIL_PORT: int = 587
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    MAIL_USERNAME: EmailStr
+    MAIL_PASSWORD: str
+    MAIL_FROM: EmailStr
+    MAIL_PORT: int
     MAIL_SERVER: str
-    MAIL_FROM_NAME:str
-    MAIL_STARTTLS: bool = True
-    MAIL_SSL_TLS: bool = False
-    USE_CREDENTIALS: bool = True
-    VALIDATE_CERTS: bool = True
+    MAIL_FROM_NAME: str
+
     DOMAIN: str
-    
+
     model_config = SettingsConfigDict(
-        env_file = ".env",
-        extra="ignore",
+        env_file=".env",
+        extra="ignore"
     )
 
 
@@ -29,3 +30,4 @@ Config = Settings()
 """broker_url = Config.REDIS_URL
 result_backend = Config.REDIS_URL"""
 broker_connection_retry_on_startup = True
+
