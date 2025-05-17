a1f847f7e025867d84d22dea58326b0699144221
Ahmoham98 <electricallover45@gmail.com>
2025-05-17 09:25:31 +0330

    adds order schema for handling order requests

---
+++ schema/order.py
@@ -0,0 +1,46 @@
+from typing import List
+from datetime import datetime
+from pydantic import BaseModel
+import uuid
+
+class OrderItemCreate(BaseModel):
+    product_id: uuid.UUID
+    quantity: int
+    
+    class Config:
+        orm_mode = True
+
+class OrderCreate(BaseModel):
+    items: List[OrderItemCreate]
+    
+    class Config:
+        orm_mode = True
+
+class OrderResponse(BaseModel):
+    uid: uuid.UUID
+    total_price: float
+    status: str
+    created_at: datetime
+    
+    class Config:
+        orm_mode = True
+
+
+class OrderItemRead(BaseModel):
+    product_title: str
+    quantity: int
+    unit_price: float
+
+    class Config:
+        orm_mode = True
+
+class OrderReadWithItems(BaseModel):
+    uid: uuid.UUID
+    user_uid: uuid.UUID
+    total_price: float
+    status: str
+    created_at: datetime
+    items: List[OrderItemRead]
+
+    class Config:
+        orm_mode = True
